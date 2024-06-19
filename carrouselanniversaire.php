2f8c415200f3eb862e9c45c52c3a677d3f6a15e8
Adrrien04 <adrrien.chandrakumar@gmail.com>
2024-06-19 22:23:22 +0200

    tentative n°2 de responsive

--- carrouselanniversaire.php
+++ carrouselanniversaire.php
@@ -4,7 +4,7 @@ Plugin Name: Carrousel d'anniversaire
 Plugin URI: https://github.com/Adrrien04/carrouselanniversaire
 Description: Vous trouverez ici le meilleur plugin de l'histoire de wordpress, le carrousel d'anniversaire !
 Author: CHANDRAKUMAR Adrrien
-Version: 1.9
+Version: 2.0
 Author URI: https://adrrienchandrakumar.vercel.app/
 */
 
